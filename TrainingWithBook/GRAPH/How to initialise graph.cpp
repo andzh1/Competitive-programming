862d8fbd00633a2d4280f9d76bff0bbdf917edeb
Andrey Zhizhelev <andrey.zhizhelev@yandex.ru>
2020-12-10 20:32:38 +0300

    Update how to...

--- TrainingWithBook/GRAPH/How to initialise graph.cpp
+++ TrainingWithBook/GRAPH/How to initialise graph.cpp
@@ -1,8 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std; 
 
-
-
 int main(){
     int n = 777;
     vector <int> adj1[n]; //array of "adjacencies" - edges of our graph
@@ -14,9 +12,7 @@ int main(){
     vector <pair<int,int>> adj2[n]; //adj[i] - {x,y}, x - # of vertex, y - weight of edge i,x
     adj2[2].push_back({4,4});// in our graph we can find edge (2;4) with weight 4
     int s;//в цикле обходим все ребра, которые выходят из вершины с номером s
-    for(auto u: adj[s]){
-        //обрабатываем 
-    }
+    
 
     /*МАТРИЦА СМЕЖНОСТИ*/
     int matrixOfAdj[n][n];
@@ -27,11 +23,5 @@ int main(){
     //if there is an edge (i;j), we add it as a pair of vertexes to our list
     //if edges in our graph have a weight, we can extend our list:
     vector <tuple<int,int,int>> edges1;//{i,j,weight}
-
-
-
-
-
-
 
 }
